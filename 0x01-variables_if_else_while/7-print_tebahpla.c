5074b5dc6a634933477a8b120567f48e0807c9f1
micheal odusanya <mikeayo52@gmail.com>
2022-09-22 23:26:13 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabets in lowercase and  reverse order.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
